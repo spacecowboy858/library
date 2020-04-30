d82b732e3c532e61371b05a547e756d4cc0121d5
Paul Nguyen <paulnguyen13127@gmail.com>
2020-04-29 18:42:33 -0700

    Completed part 5 Templating Engines

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
