f8d3c1fc367fd79ed393dca09a577cf93c9a52cd
Paul Nguyen <paulnguyen13127@gmail.com>
2020-05-11 22:58:42 -0700

    Finished Databases - Input Parameters

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+  user: 'admin-library',
+  password: 'Forte_1234',
+  server: 'pslibrary-01.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'PSLibrary',
+
+  // options: {
+  //   encrypt: true
+  // }
+};
+sql.connect(config).catch((err) => debug(err));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,80 +1,40 @@
 const express = require('express');
 
 const bookRouter = express.Router();
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Misérables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
-      title: 'A Journey into the Center of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    },
-    {
-      title: 'Life On The Mississippi',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: false
-    },
-    {
-      title: 'Childhood',
-      genre: 'Biography',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    }
-  ];
-
   bookRouter.route('/')
     .get((req, res) => {
-      res.render('bookListView', {
-        nav,
-        title: 'Library',
-        books
-      });
+      (async function query() {
+        const request = new sql.Request();
+        const { recordset } = await request.query('select * from books');
+        res.render('bookListView', {
+          nav,
+          title: 'Library',
+          books: recordset
+        });
+      }());
     });
 
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
-      res.render('bookView', {
-        nav,
-        title: 'Library',
-        book: books[id]
-      });
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request.input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        debug(recordset);
+        res.render('bookView', {
+          nav,
+          title: 'Library',
+          book: recordset[0]
+        });
+      }());
     });
 
   return bookRouter;
 }
-
 
 module.exports = router;
